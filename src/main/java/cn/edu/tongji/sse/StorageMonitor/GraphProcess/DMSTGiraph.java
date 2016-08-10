67734797c43e3ef48239faa18bf5307ce31ff673
nelson <chenran199104@163.com>
2016-08-10 19:29:47 +0800

    add DMST giraph

--- src/main/java/cn/edu/tongji/sse/StorageMonitor/GraphProcess/DMSTGiraph.java
+++ src/main/java/cn/edu/tongji/sse/StorageMonitor/GraphProcess/DMSTGiraph.java
@@ -3,8 +3,6 @@ package cn.edu.tongji.sse.StorageMonitor.GraphProcess;
 import cn.edu.tongji.sse.StorageMonitor.GraphDataSource.GraphDataSet;
 import cn.edu.tongji.sse.StorageMonitor.GraphDataSource.GraphEdge;
 import cn.edu.tongji.sse.StorageMonitor.GraphDataSource.GraphNode;
-import cn.edu.tongji.sse.StorageMonitor.GraphDataSource.Neo4j.Neo4jGraphDataSet;
-
 import java.io.*;
 import java.util.Arrays;
 import java.util.Collection;
@@ -24,8 +22,8 @@ public class DMSTGiraph implements AlgorithmTask{
      */
     @Override
     public void prepare(GraphDataSet dataset) {
-        System.out.println("start dmst giraph prepare");
-        boolean isFirstEdge = true;
+        System.out.println("start DMST giraph prepare");
+        boolean isFirstEdge;
         Iterator<GraphNode> it = dataset.iterator();
         File file = new File(INPUT_PATH);
         FileWriter fw = null;
@@ -34,38 +32,44 @@ public class DMSTGiraph implements AlgorithmTask{
             fw = new FileWriter(file);
             writer = new BufferedWriter(fw);
             while(it.hasNext()){
-                writer.write("[");
                 GraphNode nextNode = it.next();
-                writer.write(nextNode.getId() + ",0,");//vertex id and value
-                //edges
-                writer.write("[");
-                Collection<GraphEdge> outEdges = nextNode.getOutEdges();
-                Iterator<GraphEdge> edgesIterator = outEdges.iterator();
-                while (edgesIterator.hasNext()){
-                    if(isFirstEdge == true){
-                        GraphNode end = edgesIterator.next().getEnd();
-                        writer.write("[" + end.getId() + ",1]");
-                        isFirstEdge = false;
+                if (nextNode.getOutEdges().size() != 0){
+                    isFirstEdge = true;
+                    writer.write("[");
+                    writer.write(nextNode.getId() + ",1,");//vertex id and value
+                    //edges
+                    writer.write("[");
+                    Collection<GraphEdge> outEdges = nextNode.getOutEdges();
+                    Iterator<GraphEdge> edgesIterator = outEdges.iterator();
+                    while (edgesIterator.hasNext()){
+                        if(isFirstEdge == true){
+                            GraphNode end = edgesIterator.next().getEnd();
+                            writer.write("[" + end.getId() + ",1]");
+                            isFirstEdge = false;
+                        }
+                        else{
+                            writer.write(",");
+                            GraphNode end = edgesIterator.next().getEnd();
+                            writer.write("[" + end.getId() + ",1]");
+                        }
                     }
-                    else{
-                        writer.write(",");
-                        GraphNode end = edgesIterator.next().getEnd();
-                        writer.write("[" + end.getId() + ",1]");
+                    writer.write("]]");
+                    if(it.hasNext()){
+                        writer.newLine();//换行
                     }
-                }
-                writer.write("]]");
-                if(it.hasNext()){
-                    writer.newLine();//换行
                 }
             }
-            System.out.println("finish dmst giraph prepare");
+            System.out.println("finish DMST giraph prepare");
             writer.flush();
             Execute.exec("~/hadoop/hadoop-0.20.203.0/bin/hadoop fs -rmr /input/" + INPUT_NAME);
-            System.out.println("finish dmst giraph rmr input");
-            Execute.exec("~/hadoop/hadoop-0.20.203.0/bin/hadoop fs -put " + INPUT_PATH  + " /input");
-            System.out.println("finish dmst giraph put");
+            System.out.println("finish DMST giraph rmr input");
+            Execute.exec("~/hadoop/hadoop-0.20.203.0/bin/hadoop fs -put " + INPUT_PATH + " /input");
+            System.out.println("finish DMST giraph put");
             Execute.exec("~/hadoop/hadoop-0.20.203.0/bin/hadoop fs -rmr /output/DMSTOutput");
-            System.out.println("finish dmst giraph rmr out");
+            System.out.println("finish DMST giraph rmr output");
+            Execute.exec("export HADOOP_CLASSPATH=~/giraph/giraph/giraph-examples/target/giraph-examples-1.2.0-SNAPSHOT-for-hadoop-1.2.1-jar-with-dependencies.jar:~/giraph/giraph/giraph-examples/target/DMST.jar");
+            Execute.exec("export LIBJARS=~/giraph/giraph/giraph-examples/target/giraph-examples-1.2.0-SNAPSHOT-for-hadoop-1.2.1-jar-with-dependencies.jar,~/giraph/giraph/giraph-examples/target/DMST.jar");
+            System.out.println("finish DMST giraph export");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }catch (IOException e) {
@@ -82,15 +86,16 @@ public class DMSTGiraph implements AlgorithmTask{
 
     @Override
     public void run() {
-
         Execute.exec("~/hadoop/hadoop-0.20.203.0/bin/hadoop jar " +
                 "~/giraph/giraph/giraph-examples/target/giraph-examples-1.2.0-SNAPSHOT-for-hadoop-1.2.1-jar-with-dependencies.jar " +
-                "org.apache.giraph.GiraphRunner org.apache.giraph.examples.MinSpanningTree " +
+                "org.apache.giraph.GiraphRunner " +
+                "-libjars ${LIBJARS} " +
+                "MinSpanningTree " +
                 "-vif org.apache.giraph.io.formats.JsonLongDoubleFloatDoubleVertexInputFormat " +
                 "-vip /input/DMSTInput -vof org.apache.giraph.io.formats.IdWithValueTextOutputFormat " +
                 "-op /output/DMSTOutput " +
                 "-w 1 " +
-                "-mc org.apache.giraph.examples.MinSpanningTree\\MinSpanningTreeMasterCompute");
+                "-mc MinSpanningTree\\$MinSpanningTreeMasterCompute");
         System.out.println("finish DMST giraph jar");
     }
 
